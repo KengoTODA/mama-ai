e496792604b9d577c155dcaea9e1c12dc76b4312
Kengo TODA <skypencil@gmail.com>
2017-11-05 10:41:47 +0800

    submit air quality data to IFTTT webhook

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 extern crate iron;
 extern crate router;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate hyper;
+extern crate hyper_native_tls;
 
 use std::env;
 use std::io::Read;
 use iron::{Iron, Request, Response, IronResult};
 use router::Router;
 use iron::status;
+use hyper::{Client};
+use hyper::net::HttpsConnector;
+use hyper::header::Connection;
+use hyper_native_tls::NativeTlsClient;
 
 // Serves a string to the user.  Try accessing "/".
 fn hello(_: &mut Request) -> IronResult<Response> {
@@ -18,6 +27,9 @@ fn parse(req: &mut Request) -> IronResult<Response> {
     let mut buffer = String::new();
     req.body.read_to_string(&mut buffer).unwrap();
     let quality = parse_air_quality(&buffer).unwrap();
+    if quality >= 100 {
+      try_submit(quality);
+    }
     let resp = Response::with((status::Ok, format!("Air quality is {}!", quality)));
     Ok(resp)
 }
@@ -48,6 +60,29 @@ fn test_parse_no_data() {
     None);
 }
 
+fn try_submit(quality: i32) {
+  match env::var("IFTTT_KEY") {
+    Ok(key) => {
+      let url = format!("https://maker.ifttt.com/trigger/mama_ai_air_quality/with/key/{}?value1={}", key, quality);
+      submit(&url);
+    },
+    Err(e) => warn!("Failed to get IFTTT key: {}", e)
+  }
+}
+
+// https://stackoverflow.com/a/14189088/814928
+fn submit(url: &str) {
+  info!("submit to {}", url);
+  let tls = NativeTlsClient::new().unwrap();
+  let connector = HttpsConnector::new(tls);
+  let client = Client::with_connector(connector);
+
+  client.get(url)
+      .header(Connection::close())
+      .send()
+      .unwrap();
+}
+
 /// Look up our server port number in PORT, for compatibility with Heroku.
 fn get_server_port() -> u16 {
     env::var("PORT").ok().and_then(|p| p.parse().ok()).unwrap_or(8080)
@@ -55,6 +90,9 @@ fn get_server_port() -> u16 {
 
 /// Configure and run our server.
 fn main() {
+    env_logger::init().unwrap();
+    info!("starting up");
+
     // Set up our URL router.
     let mut router: Router = Router::new();
     router.get("/", hello, "index");
