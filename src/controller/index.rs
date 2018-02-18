4d3afd78a917ce518eb2f96f01ba947fd8dcf992
Kengo TODA <skypencil@gmail.com>
2018-02-18 15:33:26 +0800

    execute rustfmt
    https://github.com/rust-lang-nursery/rustfmt

--- src/controller/index.rs
+++ src/controller/index.rs
@@ -1,9 +1,12 @@
-use iron::{Request, Response, IronResult};
-use ::registry;
+use iron::{IronResult, Request, Response};
+use registry;
 use iron::status;
 
 pub fn index(_: &mut Request) -> IronResult<Response> {
     let r = registry::connect();
-    let resp = Response::with((status::Ok, format!("Latest data: {:?}", r.select().unwrap().unwrap())));
+    let resp = Response::with((
+        status::Ok,
+        format!("Latest data: {:?}", r.select().unwrap().unwrap()),
+    ));
     Ok(resp)
 }
